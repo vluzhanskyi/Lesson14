b28c5344637c808674ba3b273dff39876a2eb349
Viacheslav Luzhanskyi <vluzhanskyi@gmail.com>
2016-09-19 19:55:14 +0300

    WeatherAPI

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weather w = new Weather();
+            w.GetCurrentWeather("Chicago");
+            w.GetForeCustWeather("Chicago");
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Weather.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace lesson13
+{
+    class Weather
+    {
+        private const string AppId = "0949ad752887b4deeaf01429a455e7ed";
+        public XmlDocument  GetCurrentWeather(string city)
+        {
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}&mode=xml&units=metric", city, AppId);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(url);
+            doc.Save("Current.xml");
+            return doc;       
+        }
+
+        public XmlDocument GetForeCustWeather(string city)
+        {
+            string url = string.Format("http://api.openweathermap.org/data/2.5/forecast?q={0}&APPID={1}&mode=xml&units=metric", city, AppId);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(url);
+            doc.Save("ForeCust.xml");
+            return doc;
+        }
+    }
+}
