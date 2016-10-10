6bfbce4b330a783632e70d4b628683c8474140ad
Viacheslav Luzhanskyi <vluzhanskyi@gmail.com>
2016-10-10 13:09:26 +0300

    ref

--- WeatherClient/MVVM/WeatherViewModel.cs
+++ WeatherClient/MVVM/WeatherViewModel.cs
@@ -72,7 +72,7 @@ namespace WeatherClient.MVVM
                 {
                     data.GetWeather();
                     UpdateResults();
-                }));
+                } ));
             }
         }
     }
